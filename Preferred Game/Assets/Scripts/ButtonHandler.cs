afac1a0a1963d79d08513d9b6cfe353d5f228798
Keith Connors <c17507803@mydit.ie>
2019-02-02 00:15:33 +0000

    House Keeping
    Button Handler, Scene Selection menu and UI

---
+++ Preferred Game/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    /*
+     * Scene Order:
+     * TESTING SCENE: 0
+     * SCENE SELECT: 1
+     * KOTH: 2
+     * PROP HUNT: 3
+     * ZOMBIE: 4
+     * 
+     * */
+    public void loadKOTH()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void loadPropHunt()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void loadZombieWaveShooter()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    private void Start()
+    {
+        
+    }
+}
