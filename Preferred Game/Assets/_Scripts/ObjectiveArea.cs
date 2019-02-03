543820f2a4409f3a3399e69308ed7e4a6a849f73
Keith Connors <c17507803@mydit.ie>
2019-02-03 16:42:04 +0000

    Progress, Networking, KOTH

---
+++ Preferred Game/Assets/_Scripts/ObjectiveArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveArea : MonoBehaviour
+{
+    public GameObject ProgressBar;
+
+    private void Start()
+    {
+        //ProgressBar.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            ProgressBar.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            ProgressBar.SetActive(false);
+        }
+    }
+}
---
+++ Preferred Game/Assets/_Scripts/ProgressBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Transform LoadingBar;
+    public Transform TextPercentage;
+    public Transform TextLoading;
+
+    [SerializeField] float m_loadAmount;
+    [SerializeField] float m_loadSpeed;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            if (m_loadAmount < 100)
+            {
+                m_loadAmount += m_loadSpeed * Time.deltaTime;
+                TextPercentage.GetComponent<Text>().text = ((int)m_loadAmount).ToString() + "%";
+                TextLoading.gameObject.SetActive(true);
+            }
+            else
+            {
+
+                TextLoading.gameObject.SetActive(false);
+            }
+            LoadingBar.GetComponent<Image>().fillAmount = m_loadAmount / 100;
+        }
+    }
+}
